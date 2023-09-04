1000419172c360afa504ce7a0f91d3681cf22dd5
rileyworthen <rileyworthen.123@gmail.com>
2023-09-04 11:11:51 -0700

    removing build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rileyworthen/repos/XtraScript
-# Build directory: /home/rileyworthen/repos/XtraScript/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
